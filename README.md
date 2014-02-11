bef4ef9eda1ae23a12df1e8f96f0c1a784281551
Liso Gallo <liso@riseup.net>
2014-02-11 02:19:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Bitstamp Bitcoin Exchange Rate v1.0
+## OpenCart Extension
+### by John Atkinson (jga) from BTC Gear (http://www.btcgear.com)
+### changed to use Bitstamp BTC price by Lisandro Gallo (twitter.com/lisogallo)
+
+This OpenCart extension initializes the Bitcoin (BTC) currency inside OpenCart and updates the exchange rate every minute to remain current. It calculates the average trade value (from high and low values obtained) or pulls the last trade value, all from Bitstamp, and uses whichever one is friendlier for the merchant thus lessening the currency risk.
+
+This extension only works with USD currency supported by the Bitstamp API.
+
+### Compatible with OpenCart versions:
+
+* 1.5.0
+* 1.5.0.2
+* 1.5.0.3
+* 1.5.0.4
+* 1.5.0.5
+* 1.5.1
+* 1.5.1.1
+* 1.5.1.2
+* 1.5.1.3
+* 1.5.2
+* 1.5.2.1
+* 1.5.3.1
+* 1.5.4
+* 1.5.4.1
+* 1.5.5
+* 1.5.5.1
+* 1.5.6
+* 1.5.6.1
+
+If you have any issues or comments about this software, please contact: liso [at] riseup (dot) net
+
+AUTHOR:
+Lisandro Gallo (lisogallo) - Email: liso [at] riseup (dot) net
+
+--
+
+Based on the Bitcoin Exchange Rate v1.2 by John Atkinson (jga) from BTC Gear
+https://github.com/btcgear/Bitcoin_Exchange_Rate_OpenCart
+
+Copyright (c) 2013 John Atkinson (jga)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ upload/catalog/controller/common/bitcoin_update.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+
+AUTHOR:
+Lisandro Gallo (lisogallo) - Email: liso [at] riseup (dot) net
+
+Based on the Bitcoin Exchange Rate v1.2 by John Atkinson (jga) from BTC Gear
+https://github.com/btcgear/Bitcoin_Exchange_Rate_OpenCart
+
+Copyright (c) 2013 John Atkinson (jga)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+class ControllerCommonBitcoinUpdate extends Controller {
+	public function index() {
+		if (extension_loaded('curl')) {
+			$data = array();
+			$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code = 'BTC'");
+
+			if(!$query->row) {
+				$this->db->query("INSERT INTO " . DB_PREFIX . "currency (title, code, symbol_right, decimal_place, status) VALUES ('Bitcoin', 'BTC', ' BTC', '4', '1')");
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code = 'BTC'");
+			}
+
+			$format = '%Y-%m-%d %H:%M:%S';
+			$last_string = $query->row['date_modified'];
+			$current_string = strftime($format);
+			$last_time = strptime($last_string,$format);
+			$current_time = strptime($current_string,$format);
+
+			$num_seconds = 60; //every [this many] seconds, the update should run.
+			try{
+				if($last_time['tm_year'] != $current_time['tm_year']) {
+					$this->runUpdate();
+				}
+				else if($last_time['tm_yday'] != $current_time['tm_yday']) {
+					$this->runUpdate();
+				}
+				else if($last_time['tm_hour'] != $current_time['tm_hour']) {
+					$this->runUpdate();
+				}
+				else if(($last_time['tm_min']*60)+$last_time['tm_sec'] + $num_seconds < ($current_time['tm_min'] * 60) + $current_time['tm_sec']) {
+					$this->runUpdate();
+				}
+			}
+			catch (Exception $e) {
+				//
+			}
+		}
+	}
+
+	public function runUpdate() {
+		$path = "ticker/";
+		$req = array();
+
+		// API settings
+		$key = '';
+		$secret = '';
+
+		// generate a nonce as microtime, with as-string handling to avoid problems with 32bits systems
+		$mt = explode(' ', microtime());
+		$req['nonce'] = $mt[1].substr($mt[0], 2, 6);
+
+		// generate the POST data string
+		$post_data = http_build_query($req, '', '&');
+
+		// generate the extra headers
+		$headers = array(
+			'Rest-Key: '.$key,
+			'Rest-Sign: '.base64_encode(hash_hmac('sha512', $post_data, base64_decode($secret), true)),
+		);
+
+		// our curl handle (initialize if required)
+		static $ch = null;
+		if (is_null($ch)) {
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; Bitstamp PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
+		}
+		curl_setopt($ch, CURLOPT_URL, 'https://www.bitstamp.net/api/'.$path);
+
+		// run the query
+		$res = curl_exec($ch);
+		if ($res === false) throw new Exception('Could not get reply: '.curl_error($ch));
+		$dec = json_decode($res, true);
+		if (!$dec) throw new Exception('Invalid data received, please make sure connection is working and requested API exists');
+		$btcdata = $dec;
+
+		$currency = "BTC";
+		$high_value = $btcdata['high'];
+		$low_value = $btcdata['low'];
+
+		$avg_value = ((float)$high_value + (float)$low_value) / 2;
+		$last_value = $btcdata['last'];
+
+		if ((float)$avg_value && (float)$last_value) {
+			if($avg_value < $last_value) {
+				$value = $avg_value;
+			}
+			else {
+				$value = $last_value;
+			}
+			$value = 1/$value;
+			$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '" . (float)$value . "', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($currency) . "'");
+		}
+
+		$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '1.00000', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($this->config->get('config_currency')) . "'");
+		$this->cache->delete('currency');
+	}
+}
+?>
